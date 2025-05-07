768a7b19d29de7fc27cffecc6999f4b145caeda1
shafnavk70@gmail.com <shafnavk70@gmail.com>
2025-05-07 17:15:23 +0530

    new change

--- interio/settings.py
+++ interio/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+from decouple import config
+
 
 load_dotenv()
 
@@ -98,10 +101,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DB_URL'))
 }
 
 
